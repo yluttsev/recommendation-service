cd33220f8889f5aa79374965d1ba7616635f1d80
Yuri Luttsev <floomyz@vk.com>
2025-04-08 23:37:59 +0300

    Добавлен документ Product

---
+++ src/main/java/ru/example/recommendationservice/document/Product.java
@@ -0,0 +1,40 @@
+package ru.example.recommendationservice.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+
+@Document(collection = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
+@EqualsAndHashCode
+public class Product {
+
+    @Id
+    private Long id;
+
+    @Field(name = "name")
+    private String name;
+
+    @Field(name = "price")
+    private BigDecimal price;
+
+    @Field(name = "category")
+    private Long category;
+
+    @Field(name = "views")
+    private Integer views;
+}
