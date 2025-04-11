a1d667440b3cda80622c6237562c3d44a59f3ea4
Yuri Luttsev <floomyz@vk.com>
2025-04-11 11:40:01 +0300

    Удалены комментарии

--- src/main/java/ru/example/recommendationservice/service/RecommendationService.java
+++ src/main/java/ru/example/recommendationservice/service/RecommendationService.java
@@ -20,24 +20,16 @@ public class RecommendationService {
     private static final int TOP_LIMIT = 3;
 
     public List<ProductDto> getPersonalizedRecommendationsForUserById(Long userId) {
-        // История покупок
         List<PurchaseDto> clientPurchaseHistory = userServiceClient.getPurchaseHistoryByUserId(
                 userId,
                 0,
                 DEFAULT_PAGE_SIZE
         );
-        // Извлекаем уникальные купленные в HashSet
         Set<ProductDto> purchaseProducts = clientPurchaseHistory.stream()
                 .flatMap(purchaseDto -> purchaseDto.products().stream())
                 .collect(Collectors.toCollection(HashSet::new));
-
-        // Находим ТОП-3 категории продуктов у пользователя
         List<Long> topCategories = findTopCategories(purchaseProducts);
-
-        // По ТОП-3 товара из каждой категории
         List<ProductDto> recommendedProducts = productService.getTopProductsByCategories(topCategories, TOP_LIMIT);
-
-        // Фильтруем уже купленные товары при возврате
         return recommendedProducts.stream()
                 .filter(p -> !purchaseProducts.contains(p))
                 .toList();
